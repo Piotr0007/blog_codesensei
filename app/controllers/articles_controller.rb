e2a77e7db0b81925ebf61af0b82e7690d30c11c1
Piotr0007 <piozajac@onet.pl>
2018-04-28 14:08:37 +0200

    Create validation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,14 +3,16 @@ class ArticlesController < ApplicationController
   	@articles =Article.all
   end
   def new
+  	@article = Article.new
   end
   def create
   	articles_params = params.require(:article).permit(:title, :text)
   	@article = Article.new(articles_params)
-  	@article.save
-  	
-
-  	redirect_to article_path(@article)
+  	if @article.save
+  	  redirect_to article_path(@article)
+  	else
+  	  render 'new' 
+  	end   
   end 
   def show
   	@article = Article.find(params[:id])
