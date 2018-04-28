0c4d0f20fb08aa527ad19ec76f78e0a2be177090
Piotr0007 <piozajac@onet.pl>
2018-04-28 13:38:31 +0200

    Create links between actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,7 +10,7 @@ class ArticlesController < ApplicationController
   	@article.save
   	
 
-  	redirect_to articles_path
+  	redirect_to article_path(@article)
   end 
   def show
   	@article = Article.find(params[:id])
